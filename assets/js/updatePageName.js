7f662fd4aea0c805867ef28716b4c9cc8bf84689
zipper3030 <zlloyd@gradcenter.cuny.edu>
2023-10-18 17:02:08 -0400

    new and improved FAQ page + various other fixes

--- assets/js/updatePageName.js
+++ assets/js/updatePageName.js
@@ -1,15 +1,20 @@
-// get the current page url and name and update the page name in the header
-
 document.addEventListener("DOMContentLoaded", function () {
-    let currentPageURL = window.location.pathname;
-  
-    currentPageURL = currentPageURL.replace(/\/$/, '');
-  
-    let pageName = currentPageURL.split("/").pop().replace(".html", "");
-    pageName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
-  
-    let currentPageNameElement = document.getElementById("currentPageName");
-    if (currentPageNameElement) {
-      currentPageNameElement.textContent = pageName;
-    }
-  });
+  let currentPageURL = window.location.pathname;
+
+  currentPageURL = currentPageURL.replace(/\/$/, '');
+
+  let pageName = currentPageURL.split("/").pop().replace(".html", "");
+  pageName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
+
+  //changing faq doesn't seem to be working for some reason, unsure why :(
+  if (pageName.toLowerCase() === "faq") {
+    pageName = "Frequently Asked Questions";
+    console.log("pageName: " + pageName);
+  }
+
+  let currentPageNameElement = document.getElementById("currentPageName");
+  if (currentPageNameElement) {
+    currentPageNameElement.textContent = pageName;
+  }
+});
+
