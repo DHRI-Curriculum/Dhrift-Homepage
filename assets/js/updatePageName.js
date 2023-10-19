ef141cb16d31780423f59afc40c3085133dfba81
zipper3030 <zlloyd@gradcenter.cuny.edu>
2023-10-19 17:24:02 -0400

    header alignment and other fixes

--- assets/js/updatePageName.js
+++ assets/js/updatePageName.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
   let pageName = currentPageURL.split("/").pop().replace(".html", "");
   pageName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
 
-  //changing faq doesn't seem to be working for some reason, unsure why :(
   if (pageName.toLowerCase() === "faq") {
     pageName = "Frequently Asked Questions";
     console.log("pageName: " + pageName);
